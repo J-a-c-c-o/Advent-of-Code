8710a84ab30c5d3f260cefd39bcf0e6b97d9491b
Jacco <46869236+J-a-c-c-o@users.noreply.github.com>
2023-12-14 08:21:58 +0100

    day 14 2023

---
+++ src/year2023/day14/Main.java
@@ -0,0 +1,176 @@
+package year2023.day14;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import utils.Utils;
+
+public class Main {
+
+    Utils utils = new Utils();
+
+    List<Rock> rocks;
+    int maxX = 0;
+    int maxY = 0;
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Main main = new Main();
+        String part1 = main.part1();
+        long part1Time = System.currentTimeMillis() - start;
+        String part2 = main.part2();
+        long part2Time = System.currentTimeMillis() - start - part1Time;
+
+        System.out.println("Part 1: " + part1 + " took " + part1Time + "ms");
+        System.out.println("Part 2: " + part2 + " took " + part2Time + "ms");
+    }
+
+    public void getInput() {
+        List<String> lines = utils.getLines("src/year2023/day14/input.txt");
+
+        maxY = lines.size();
+        maxX = lines.get(0).length();
+
+        rocks = new ArrayList<>();
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            char[] chars = line.toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                char c = chars[j];
+                if (c == 'O' || c == '#') {
+                    rocks.add(new Rock(j, i, c, maxX - 1, maxY - 1));
+                }
+            }
+
+        }
+
+
+    }
+
+    public String part1() {
+        getInput();
+
+        tilt(Direction.UP);
+
+
+        long total = 0;
+        for (Rock rock : rocks) {
+            if (rock.c == 'O') {
+                total += maxY - rock.getY();
+            }
+        }
+
+        return String.valueOf(total);
+    }
+
+    public String part2() {
+        getInput();
+
+        int i = 0;
+        Map<Integer, Integer> cachedRocks = new HashMap<>();
+        while (i < 1000000000) {
+
+            cycle();
+
+
+            List<Rock> copyRocks = new ArrayList<>(this.rocks);
+
+            copyRocks.sort((o1, o2) -> {
+                if (o1.y == o2.y) {
+                    return o1.x - o2.x;
+                }
+                return o1.y - o2.y;
+            });
+
+            if (cachedRocks.containsKey(copyRocks.hashCode())) {
+                System.out.println("Found cycle " + i);
+
+                int cycleStart = cachedRocks.get(copyRocks.hashCode());
+                int cycleLength = i - cycleStart;
+                int cycleOffset = (1000000000 - 1 - cycleStart) % cycleLength;
+
+                for (int j = 0; j < cycleOffset; j++) {
+                    cycle();
+                }
+                break;
+            }
+
+
+            cachedRocks.put(copyRocks.hashCode(), i);
+
+
+            i++;
+
+
+        }
+
+
+        long total = 0;
+        for (Rock rock : rocks) {
+            System.out.println(rock);
+            if (rock.c == 'O') {
+                total += maxY - rock.getY();
+            }
+        }
+
+        char[][] map = new char[maxY][maxX];
+        for (Rock rock : rocks) {
+            map[rock.getY()][rock.getX()] = rock.c;
+        }
+
+        for (char[] chars : map) {
+            System.out.println(chars);
+        }
+
+
+        return String.valueOf(total);
+    }
+
+    private void cycle() {
+        tilt(Direction.UP);
+        tilt(Direction.LEFT);
+        tilt(Direction.DOWN);
+        tilt(Direction.RIGHT);
+    }
+
+    enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    private void tilt(Direction direction) {
+        boolean done = false;
+        while (!done) {
+            List<Boolean> moved = new ArrayList<>();
+
+            for (Rock rock : rocks) {
+                if (rock.c == 'O') {
+                    switch (direction) {
+                        case UP -> moved.add(rock.up(rocks));
+                        case DOWN -> moved.add(rock.down(rocks));
+                        case LEFT -> moved.add(rock.left(rocks));
+                        case RIGHT -> moved.add(rock.right(rocks));
+                    }
+                }
+            }
+
+
+            boolean allFalse = true;
+            for (Boolean b : moved) {
+                if (b) {
+                    allFalse = false;
+                    break;
+                }
+            }
+
+            if (allFalse) {
+                done = true;
+            }
+        }
+    }
+}
