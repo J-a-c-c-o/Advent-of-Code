24d9aff1ffcc8ea31867e4d49755fdf91a66382f
Jacco <jacco@tepoel.net>
2024-10-24 00:32:03 +0200

    done 2021/5-6

---
+++ src/utils/Point.java
@@ -0,0 +1,42 @@
+package utils;
+
+import java.util.Objects;
+
+public class Point {
+
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point point)) return false;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
--- src/year2021/day5/Main.java
+++ src/year2021/day5/Main.java
@@ -4,6 +4,7 @@ package year2021.day5;
 import java.util.*;
 
 import utils.Pair;
+import utils.Point;
 import utils.Utils;
 
 public class Main {
@@ -22,15 +23,15 @@ public class Main {
         System.out.println("Part 2: " + part2 + " took " + part2Time + "ms");
     }
 
-    public List<Pair<Pair<Integer,Integer>, Pair<Integer,Integer>>> getInput(boolean part1) {
-        List<String> dirs = utils.getLines("src/year2021/day5/example.txt");
-        List<Pair<Pair<Integer,Integer>, Pair<Integer,Integer>>> pairs = new ArrayList<>();
+    public List<Pair<Point, Point>> getInput(boolean part1) {
+        List<String> dirs = utils.getLines("src/year2021/day5/input.txt");
+        List<Pair<Point, Point>> pairs = new ArrayList<>();
         for (String dir : dirs) {
             String[] temp = dir.split(" -> ");
             String[] temp2 = temp[0].split(",");
             String[] temp3 = temp[1].split(",");
-            Pair<Integer,Integer> p1 = new Pair<>(Integer.parseInt(temp2[0]), Integer.parseInt(temp2[1]));
-            Pair<Integer,Integer> p2 = new Pair<>(Integer.parseInt(temp3[0]), Integer.parseInt(temp3[1]));
+            Point p1 = new Point(Integer.parseInt(temp2[0]), Integer.parseInt(temp2[1]));
+            Point p2 = new Point(Integer.parseInt(temp3[0]), Integer.parseInt(temp3[1]));
             if (part1 && (!Objects.equals(p1.x, p2.x) && !Objects.equals(p1.y, p2.y))) {
                 continue;
             }
@@ -42,15 +43,31 @@ public class Main {
     }
 
     public String part1() {
-        List<Pair<Pair<Integer,Integer>, Pair<Integer,Integer>>> pairs = getInput(true);
+        List<Pair<Point, Point>> pairs = getInput(true);
 
-        Set<Pair<Integer,Integer>> set = new HashSet<>();
-        Set<Pair<Integer,Integer>> visited = new HashSet<>();
-        for (Pair<Pair<Integer,Integer>, Pair<Integer,Integer>> pair : pairs) {
-            Pair<Integer,Integer> current = pair.x;
-            Pair<Integer,Integer> target = pair.y;
+        return countOverlap(pairs);
+    }
 
-            while (!Objects.equals(current.x, target.x) || !Objects.equals(current.y, target.y)) {
+    public String part2() {
+        List<Pair<Point, Point>> pairs = getInput(false);
+
+        return countOverlap(pairs);
+
+    }
+
+    private static String countOverlap(List<Pair<Point, Point>> pairs) {
+        Set<Point> set = new HashSet<>();
+        Set<Point> visited = new HashSet<>();
+        for (Pair<Point, Point> pair : pairs) {
+            Point current = pair.x;
+            Point target = pair.y;
+
+            if (set.contains(current)) {
+                visited.add(new Point(current.x, current.y));
+            }
+            set.add(new Point(current.x, current.y));
+
+            while (!current.equals(target)) {
                 if (current.x < target.x) {
                     current.x++;
                 } else if (current.x > target.x) {
@@ -63,28 +80,19 @@ public class Main {
                     current.y--;
                 }
 
-
                 if (set.contains(current)) {
-                    visited.add(current);
+                    visited.add(new Point(current.x, current.y));
                 }
-                set.add(current);
+                set.add(new Point(current.x, current.y));
 
             }
         }
-
-        System.out.println(set);
 
         //filter frequency set to only include pairs that have been visited more than once
 
 
         return String.valueOf(visited.size());
-
     }
-
-    public String part2() {
-        return "";
-    }
-
 
 
 }
