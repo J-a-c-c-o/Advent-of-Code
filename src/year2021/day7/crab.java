e48d9ea22b25e984369520aeae12771516b7bb2d
Jacco <jacco@tepoel.net>
2024-10-31 02:24:56 +0100

    done 2021/7-8

---
+++ src/year2021/day7/crab.java
@@ -0,0 +1,26 @@
+package year2021.day7;
+
+public class crab {
+    private int pos;
+
+    public crab(int pos) {
+        this.pos = pos;
+    }
+
+
+    public int getPos() {
+        return pos;
+    }
+
+
+
+    public int moveTo(int pos) {
+        return Math.abs(this.pos - pos);
+    }
+
+    public int moveToP2(int pos) {
+        int steps = Math.abs(this.pos - pos);
+
+        return (steps*(steps+1))/2;
+    }
+}
