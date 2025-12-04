83209f98382bc44e13e9d6f1a2e4541108f78e00
Jacco te Poel <j.j.tepoel@student.utwente.nl>
2025-12-04 17:29:29 +0100

    day/04 2025

---
+++ src/main/java/nl/jtepoel/AOC/year2025/day4/Main.java
@@ -0,0 +1,116 @@
+package nl.jtepoel.AOC.year2025.day4;
+
+
+import java.util.List;
+
+import nl.jtepoel.AOC.utils.Grid;
+import nl.jtepoel.AOC.utils.Pair;
+import nl.jtepoel.AOC.utils.Utils;
+
+public class Main {
+
+    Utils utils = new Utils();
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Main main = new Main();
+        String part1 = main.part1();
+        long part1Time = System.currentTimeMillis() - start;
+        String part2 = main.part2();
+        long part2Time = System.currentTimeMillis() - start - part1Time;
+
+        System.out.println("Part 1: " + part1 + " took " + part1Time + "ms");
+        System.out.println("Part 2: " + part2 + " took " + part2Time + "ms");
+    }
+
+    public List<String> getInput() {
+        return utils.getLines(Utils.LOCSRC + "/year2025/day4/input.txt");
+    }
+
+    public String part1() {
+        List<String> getInput = getInput();
+        Grid<Character> grid = new Grid<>();
+        for (int i = 0; i < getInput.size(); i++) {
+            String input = getInput.get(i);
+            for  (int j = 0; j < input.length(); j++) {
+                grid.set(j,i, input.charAt(j));
+            }
+
+        }
+
+        int[][] adjacent = new int[][]{{0,1}, {0,-1}, {1,0}, {-1,0}, {-1,-1}, {1,-1}, {1,1}, {-1,1}};
+        Pair<Integer,Integer> dim = grid.getDimensions();
+        int sum = 0;
+        for (int i = 0; i <= dim.x; i++) {
+            for (int j = 0; j <= dim.y; j++) {
+
+                if (grid.get(j, i) != '@') continue;
+
+                int count = 0;
+                for (int[] dir : adjacent) {
+                    if (grid.getOrDefault(j + dir[0], i + dir[1], '.') == '@') {
+                        count++;
+                    }
+                }
+
+                if (count < 4) {
+                    sum++;
+                }
+            }
+        }
+
+
+
+
+        return String.valueOf(sum);
+    }
+
+    public String part2() {
+        List<String> getInput = getInput();
+        Grid<Character> grid = new Grid<>();
+        for (int i = 0; i < getInput.size(); i++) {
+            String input = getInput.get(i);
+            for  (int j = 0; j < input.length(); j++) {
+                grid.set(j,i, input.charAt(j));
+            }
+
+        }
+
+        int[][] adjacent = new int[][]{{0,1}, {0,-1}, {1,0}, {-1,0}, {-1,-1}, {1,-1}, {1,1}, {-1,1}};
+        Pair<Integer,Integer> dim = grid.getDimensions();
+        int total = 0;
+        while(true) {
+            int sum = 0;
+            for (int i = 0; i <= dim.x; i++) {
+                for (int j = 0; j <= dim.y; j++) {
+
+                    if (grid.get(j, i) != '@') continue;
+
+                    int count = 0;
+                    for (int[] dir : adjacent) {
+                        if (grid.getOrDefault(j + dir[0], i + dir[1], '.') == '@') {
+                            count++;
+                        }
+                    }
+
+                    if (count < 4) {
+                        sum++;
+                        grid.set(j, i, '.');
+                    }
+                }
+            }
+
+            if (sum == 0) {
+                break;
+            }
+
+            total += sum;
+
+
+        }
+        return String.valueOf(total);
+    }
+
+
+
+}
